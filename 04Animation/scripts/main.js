800e33b55c2b3163373b61fa01751d79284b26c7
HesperianDragon <42208828+HesperianDragon@users.noreply.github.com>
2020-05-22 21:53:29 +0200

    Test

--- 04Animation/scripts/main.js
+++ 04Animation/scripts/main.js
@@ -1,7 +1,7 @@
 
 
 // // changing individual properties with code and using setInterval
-var rotationSpeed = 0.1;
+var rotationSpeed = 1.00;
 var myOtherBox = document.getElementById('myOtherBox');
 
 function spin(){
@@ -10,5 +10,3 @@ function spin(){
 }
 
 setInterval(spin, 16); //equivalent to 60 fps
-
-// setInterval(spin, 16); //equivalent to 60 fps
