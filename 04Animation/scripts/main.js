35a9a717d1c8880f4d2e32ff7a5297b0442d51e6
HesperianDragon <42208828+HesperianDragon@users.noreply.github.com>
2020-05-22 21:38:35 +0200

    FASTER FASTER

--- 04Animation/scripts/main.js
+++ 04Animation/scripts/main.js
@@ -1,7 +1,7 @@
 
 
 // // changing individual properties with code and using setInterval
-var rotationSpeed = 0.01;
+var rotationSpeed = 0.1;
 var myOtherBox = document.getElementById('myOtherBox');
 
 function spin(){
@@ -9,6 +9,6 @@ function spin(){
 	console.log(myOtherBox.object3D.rotation.x);
 }
 
-setInterval(spin, 32); //equivalent to 60 fps
+setInterval(spin, 16); //equivalent to 60 fps
 
 // setInterval(spin, 16); //equivalent to 60 fps
